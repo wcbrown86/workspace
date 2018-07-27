f2bc08e0b914fce8da9f2973dfe8d3f57c5b69d2
William Brown <brownwc86@gmail.com>
2018-07-27 02:56:32 -0400

    Created Test file in Ruby folder, only used for learning.

---
+++ Ruby/Test.rb
@@ -0,0 +1,8 @@
+# Test File, used to help with the learning process and to see the output of 
+# simple commands to be used in other programs. 
+
+
+
+puts (3 < 4 || false) && (false || true)
+puts  !true && (!true || 100 != 5**2)
+puts true || !(true || false)
