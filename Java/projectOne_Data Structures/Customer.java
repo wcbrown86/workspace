2f2241414d97da6a7684b2f6a322c9b49a580e6e
William Brown <brownwc86@gmail.com>
2018-08-11 06:11:41 -0400

    Finished commenting projectOne_DataStructures

--- Java/projectOne_Data Structures/Customer.java
+++ Java/projectOne_Data Structures/Customer.java
@@ -1,24 +1,67 @@
+/**
+ * 
+ * @author William Chad Brown
+ * 
+ * Description:	The Customer Class holds the information about the 
+ * 				next customer in line, as well as the service time for 
+ * 				the customer. The service time is decremented each loop,
+ * 				until it is equal to 0. Then at that time the customer is
+ * 				removed from the queue.
+ *
+ */
 
 public class Customer {
 	
+	//Global variables that hold the next customer in the queue and the service time
+	// which is currently set at 5. 
 	Customer next;
 	int serviceTime = 5;
 
+	/**
+	 * 
+	 * The set next function, sets the next variable to a customer object
+	 * that points the to customer that is next in line in the queue. 
+	 * 
+	 * @param c - Customer object that represents the next customer in line. 
+	 * 
+	 */
 	public void setNext(Customer c) {
-		// TODO Auto-generated method stub
 		next = c;
 	}
 
+	/**
+	 * 
+	 * A get function that returns the value of next, a customer object 
+	 * that represents the next customer in line. 
+	 * 
+	 * @return - a Customer object that represents the next customer in line
+	 * 
+	 */
 	public Customer getNext() {
 		// TODO Auto-generated method stub
 		return next;
 	}
 
+	/**
+	 * 
+	 * A get function for the service time of the customer, this returns an 
+	 * integer that represents the remaining service time for this customer.
+	 * 
+	 * @return - a integer that represents the remaining service time for the 
+	 * 			 customer. 
+	 * 
+	 */
 	public int getServiceTime() {
 		// TODO Auto-generated method stub
 		return serviceTime;
 	}
 
+	/**
+	 * 
+	 * A function that will reduce the customer service time by one. This is 
+	 * used to simulate one minute passing. 
+	 * 
+	 */
 	public void decServiceTime() {
 		// TODO Auto-generated method stub
 		serviceTime--;
--- Java/projectOne_Data Structures/Queue.java
+++ Java/projectOne_Data Structures/Queue.java
@@ -1,15 +1,46 @@
+/**
+ * 
+ * @author - William Chad Brown
+ * 
+ * Description:	The Queue class is a linked list that implements a first in 
+ * 				first out queue. This class is set up to only support a Customer
+ * 				class object. 
+ *
+ */
+
 public class Queue{
     
+	// Global variables the store the head and tail of the list. 
     public Customer first; 
     public Customer last; 
+    
+    // Count variable that keeps track of how many items are in the list. 
     public int count;
     
+    
+    /**
+     * 
+     * Constructor that sets up an empty list. It sets
+     * the first and last variable to null and count to 0.
+     * 
+     */
     public Queue(){
         first = null;
         last = null;
         count = 0;
     }
     
+    /**
+     * 
+     * Add Customer function will add an item to the linked list.
+     * If the list in empty the first variable is set to the added 
+     * Customer object, and since this is a FIFO queue all new items 
+     * are added to the end of the list, so the last variable is set
+     * to the passed Customer object. 
+     * 
+     * @param c - A Customer object that needs to be added to the Queue.
+     * 
+     */
     public void addCustomer(Customer c){
         if(isEmpty())
             first = c;
@@ -20,17 +51,31 @@ public class Queue{
         count++;
     }
     
+    /**
+     * 
+     * Remove customer function, takes the first customer 
+     * Variable and point that to the second customer in line.
+     * The object is not deleted, but is removed from any memory pointers 
+     * and will be cleaned up with automatic garbage collection. 
+     * 
+     */
     public void removeCustomer(){
     
       if(!isEmpty()){
          first = first.getNext();
          count--;
-      }
-         
+      }      
     }
-    
+
+    /**
+     * 
+     * Checks to see if the list is empty. If the list is empty the function
+     * returns True otherwise False
+     * 
+     * @return - returns a boolean true if the list is empty, and false otherwise.
+     * 
+     */
     public boolean isEmpty(){
         return first == null;
-    }
-    
+    } 
 }
