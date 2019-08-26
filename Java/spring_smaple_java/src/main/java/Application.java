f585449a59b8b82070d0cb5f6d821319784f9090
William Brown <brownwc86@gmail.com>
2019-08-25 22:15:28 -0400

    Learning Spring Boot.

--- Java/spring_smaple_java/src/main/java/Application.java
+++ Java/spring_smaple_java/src/main/java/Application.java
@@ -1,13 +1,21 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import com.pluralsight.service.CustomerService;
-import com.pluralsight.service.CustomerServiceImpl;
 
 public class Application {
 	
 	public static void main(String[] args) {
 		
-		CustomerService service = new CustomerServiceImpl();
+		ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		//CustomerService service = new CustomerServiceImpl();
+		
+		CustomerService service = appContext.getBean("customerService", CustomerService.class);
 		
 		System.out.println(service.findAll().get(0).getFirstName());
+		
+		
 	}
 
 }
--- Java/spring_smaple_java/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ Java/spring_smaple_java/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -4,12 +4,21 @@ import java.util.List;
 
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
-import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 
 public class CustomerServiceImpl implements CustomerService {
 	
-	private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+	private CustomerRepository customerRepository;
 	
+	public CustomerServiceImpl (CustomerRepository customerRepository) {
+		
+		this.customerRepository = customerRepository;
+		
+	}
+	
+	public void setCustomerRepository(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+
 	@Override
 	public List<Customer> findAll(){
 		return customerRepository.findAll();
