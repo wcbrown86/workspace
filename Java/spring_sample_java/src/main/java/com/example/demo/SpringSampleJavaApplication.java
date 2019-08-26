ca5343c362741d3a895b33ee5dbc5c48e259b7b2
William Brown <brownwc86@gmail.com>
2019-08-25 21:56:57 -0400

    Updating Git.

---
+++ Java/spring_sample_java/src/main/java/com/example/demo/SpringSampleJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSampleJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSampleJavaApplication.class, args);
+	}
+
+}
