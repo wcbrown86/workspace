8efce3e06d347c38ca6b27d67b3808d33e7414c6
William Brown <brownwc86@gmail.com>
2019-09-13 23:23:00 -0400

    Working on Builder Pattern.

---
+++ Java/CreationalPatterns/src/main/java/com/pluralsight/builder/LunchOrderBean.java
@@ -0,0 +1,5 @@
+package com.pluralsight.builder;
+
+public class LunchOrderBean {
+
+}
---
+++ Java/CreationalPatterns/src/main/java/com/pluralsight/builder/LunchOrderBeanDemo.java
@@ -0,0 +1,10 @@
+package com.pluralsight.builder;
+
+public class LunchOrderBeanDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
