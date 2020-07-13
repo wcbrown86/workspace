26771c5094b4ab06e678c40a8e602a87304cc525
William Brown <brownwc86@gmail.com>
2020-07-12 20:07:01 -0400

    Completed crash course for C#.

---
+++ C#/Operators/Operators/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Globalization;
+
+namespace Operators
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            integerMath();
+            orderOfOperation();
+            comparisionOperators();
+        }
+
+        static void integerMath()
+        {
+            var a = 10;
+            var b = 3;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a / b);
+            Console.WriteLine((float)a / (float)b + "\n");
+        }
+
+        static void orderOfOperation()
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+
+            Console.WriteLine("Should be 7.");
+            Console.WriteLine(a + b * c);
+
+            Console.WriteLine("Should be 9.");
+            Console.WriteLine((a + b) * c);
+            Console.WriteLine("\n");
+
+        }
+
+        static void comparisionOperators()
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+
+            Console.WriteLine("Should be true.");
+            Console.WriteLine(a < b);
+
+            Console.WriteLine("Should be false.");
+            Console.WriteLine(a == b);
+
+            Console.WriteLine("Should be true.");
+            Console.WriteLine(a != b);
+
+            Console.WriteLine("Should be true.");
+            Console.WriteLine(c > b && c > a);
+
+            Console.WriteLine("Should be false.");
+            Console.WriteLine(c > b && c == a);
+
+            Console.WriteLine("Should be true.");
+            Console.WriteLine(c > b || c == a);
+
+            Console.WriteLine("Should be false.");
+            Console.WriteLine(!(c > b) && c > a);
+        }
+    }
+}
